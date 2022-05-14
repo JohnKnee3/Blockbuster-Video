d87865ea1f8abf17ff9651394de8c8c8a2c475a0
john.a.clark3 <john.a.clark3@gmail.com>
2022-05-14 15:49:32 -0600

    more heroku science

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,32 +1,30 @@
 import React, { useEffect } from "react";
-import { useQuery } from "@apollo/react-hooks";
-import { QUERY_CATEGORIES } from "../../utils/queries";
+import { useQuery } from "@apollo/client";
 import { useStoreContext } from "../../utils/GlobalState";
 import {
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
 } from "../../utils/actions";
+import { QUERY_CATEGORIES } from "../../utils/queries";
 import { idbPromise } from "../../utils/helpers";
 
 function CategoryMenu() {
   const [state, dispatch] = useStoreContext();
+
   const { categories } = state;
+
   const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
 
-  // Update the state with the categories upon page load or change
   useEffect(() => {
-    // if categoryData exists or has changed from the response of useQuery, then run dispatch to update the state with the UPDATE_CATEGORIES action
     if (categoryData) {
       dispatch({
         type: UPDATE_CATEGORIES,
         categories: categoryData.categories,
       });
-      // also store the category data in IndexedDB
       categoryData.categories.forEach((category) => {
         idbPromise("categories", "put", category);
       });
     } else if (!loading) {
-      // if the user is offline, load data from IndexedDB
       idbPromise("categories", "get").then((categories) => {
         dispatch({
           type: UPDATE_CATEGORIES,
