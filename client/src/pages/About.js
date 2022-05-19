09b7193f41736a33099ea3f4a9d146055d264c14
john.a.clark3 <john.a.clark3@gmail.com>
2022-05-19 10:59:47 -0600

    removed boot strap

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,72 +1,72 @@
 import React from "react";
-import { Row, Col } from "react-bootstrap";
+
 const About = () => {
   return (
     <div className="container">
-      <Row>
+      <div className="row">
         <h2>WE ARE BACK!!!!!!</h2>
-        <Col lg={6} md={12}>
+        <div className="col">
           <img
             className="about-image"
             alt="Blockbuster Logo"
             src={`/logo512.png`}
           />
-        </Col>
-        <Col lg={6} md={12}>
+        </div>
+        <div className="col">
           <p>
             I know you missed us and are even like man where have you been? Well
             let us tell you...
           </p>
-        </Col>
-      </Row>
-      <Row>
-        <Col lg={6} md={12}>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
           <p>When we went under we got really sad.</p>
-        </Col>
-        <Col lg={6} md={12}>
+        </div>
+        <div className="col">
           <img
             className="about-image"
             alt="sad no money"
             src={`/images/sad.jpeg`}
           />
-        </Col>
-      </Row>
-      <Row>
-        <Col lg={6} md={12}>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
           <img
             className="about-image"
             alt="person meditating"
             src={`/images/soul-searching.jpeg`}
           />
-        </Col>
-        <Col lg={6} md={12}>
+        </div>
+        <div className="col">
           <p>We did some soul searching.</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col lg={6} md={12}>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
           <p>We read a book.</p>
-        </Col>
-        <Col lg={6} md={12}>
+        </div>
+        <div className="col">
           <img
             className="about-image"
             alt="woman reading a book"
             src={`/images/reading-book.jpeg`}
           />
-        </Col>
-      </Row>
-      <Row>
-        <Col lg={6} md={12}>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
           <img
             className="about-image"
             alt="person meditating"
             src={`/images/farmers-market.jpeg`}
           />
-        </Col>
-        <Col lg={6} md={12}>
+        </div>
+        <div className="col">
           <p>We went to serveral farmers markets and finally....</p>
-        </Col>
-      </Row>
+        </div>
+      </div>
     </div>
   );
 };
