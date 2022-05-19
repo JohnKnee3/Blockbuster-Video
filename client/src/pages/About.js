c618d908f788ec863e7a17666600b1ae3cb2e95f
john.a.clark3 <john.a.clark3@gmail.com>
2022-05-19 10:00:49 -0600

    adding images and skeleton to the about

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,9 +1,60 @@
 import React from "react";
-
+import { Row, Col } from "react-bootstrap";
 const About = () => {
   return (
     <div className="container">
-      <h2>In development</h2>
+      <Row>
+        <h2>WE ARE BACK!!!!!!</h2>
+        <Col lg={6} md={12}>
+          <img
+            className="about-image"
+            alt="Blockbuster Logo"
+            src={`/logo512.png`}
+          />
+        </Col>
+        <Col lg={6} md={12}>
+          <p>
+            I know you missed us and are even like man where have you been? Well
+            let us tell you...
+          </p>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={6} md={12}>
+          <p>When we went under we got really sad.</p>
+        </Col>
+        <Col lg={6} md={12}>
+          <img
+            className="about-image"
+            alt="sad no money"
+            src={`/images/sad.jpeg`}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={6} md={12}>
+          <img
+            className="about-image"
+            alt="person meditating"
+            src={`/images/soul-searching.jpeg`}
+          />
+        </Col>
+        <Col lg={6} md={12}>
+          <p>We did some soul searching.</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={6} md={12}>
+          <p>We read a book.</p>
+        </Col>
+        <Col lg={6} md={12}>
+          <img
+            className="about-image"
+            alt="woman reading a book"
+            src={`/images/reading-book.jpeg`}
+          />
+        </Col>
+      </Row>
     </div>
   );
 };
