b22330a742d938184c3e3b15ee15398a79ea3f6b
john.a.clark3 <john.a.clark3@gmail.com>
2022-05-21 09:12:53 -0600

    Revert "Revert "working on footer location""
    This reverts commit 9d366993f8e05a5b941b0ac16a1799a60d7d3442.

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="px-2 py-1 text-center need-less-space">
+    <footer className="need-less-space">
+      <div className="px-2 py-1 text-center ">
         <span className="footer">
           &copy;{new Date().getFullYear()} by{" "}
           <a
